18a89f3067d95f333d561098beb945606a0194e2
ignaciofranco2003 <ignaciofranco2003@gmail.com>
2024-11-10 23:10:11 -0300

    Initial commit

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+function participar(){
+    window.location.href = "participar.html";
+}
+
+function donar(){
+    window.location.href = "donar.html";
+}
+
+function proyectos(){
+    window.location.href = "proyectos.html";
+}
