e55db2f6f68e8f4a70d44ee48b434dc021811808
ignaciofranco2003 <ignaciofranco2003@gmail.com>
2024-11-13 18:35:23 -0300

    Pagina principal ✅

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,19 @@ function donar(){
 function proyectos(){
     window.location.href = "proyectos.html";
 }
+
+// Muestra o esconde el botón según el scroll
+window.onscroll = function() {
+    const backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+// Función para volver al inicio de la página
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
